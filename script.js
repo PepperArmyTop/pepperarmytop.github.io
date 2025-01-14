8dde5faec71728f74a2aeb335c04295e5c93e472
PepperArmyTop <elpepperinogris@gmail.com>
2025-01-14 11:39:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-// Cargar datos del archivo JSON y renderizar el Top 10
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
         const list = document.getElementById('top10-list');
+        data.sort((a, b) => b.score - a.score); // Ordenar por puntaje (mayor puntaje primero)
+        
         data.forEach((user, index) => {
             const listItem = document.createElement('li');
-            listItem.innerHTML = `<strong>${index + 1}. <a href="${user.link}" target="_blank">${user.name}</a></strong> - ${user.score} pts`;
+            const medalsHTML = user.medals.map(medal => `<img src="medals/${medal}.png" alt="${medal} medal" class="medal-icon">`).join('');
+            
+            listItem.innerHTML = `
+                <strong>${index + 1}. <a href="${user.link}" target="_blank">${user.name}</a></strong> - ${user.score} pts
+                ${medalsHTML}
+            `;
             list.appendChild(listItem);
         });
     })
